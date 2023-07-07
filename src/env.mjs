51f0c43ff0072df38b30189de890b8126f8380ee
Freddy L <neo-ciber_94@hotmail.com>
2023-07-07 19:28:04 -0400

    Updated sst

--- src/env.mjs
+++ src/env.mjs
@@ -14,7 +14,9 @@ export const env = createEnv({
     OPEN_API_KEY: z.string(),
     AWS_ACCESS_KEY_ID: z.string(),
     AWS_SECRET_KEY: z.string(),
-    AWS_BUCKET_NAME: z.string()
+    AWS_BUCKET_NAME: z.string(),
+    CLERK_SECRET_KEY: z.string(),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   },
 
   /**
@@ -22,9 +24,7 @@ export const env = createEnv({
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
-  client: {
-    
-  },
+  client: {},
 
   /**
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
@@ -38,7 +38,9 @@ export const env = createEnv({
     OPEN_API_KEY: process.env.OPEN_API_KEY,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
-    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,22 @@
-import { SSTConfig } from "sst";
+import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
+import { env } from "~/env.mjs";
+
+// We need to skip the AWS keys
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, ...environment } = env;
 
 export default {
   config(_input) {
     return {
-      name: "image-generator",
+      name: "ImageGen",
       region: "us-east-1",
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "ImageGenSite", {
+        environment
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
