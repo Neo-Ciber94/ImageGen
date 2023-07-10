07452b160eb91adc1b22a7c09f5b6f4c148da3a8
Freddy L <neo-ciber_94@hotmail.com>
2023-07-10 01:28:33 -0400

    minor changes

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -6,15 +6,17 @@ export default function Page() {
   const { isSignedIn, isLoaded } = useUser();
 
   if (!isLoaded) {
-    <p className="flex w-full flex-row justify-center p-10 text-purple-500">
-      <ThreeDots
-        height="80"
-        width="80"
-        radius="9"
-        color="rgb(168 85 247)"
-        ariaLabel="loading"
-      />
-    </p>;
+    return (
+      <p className="flex w-full flex-row justify-center p-10 text-purple-500">
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="rgb(168 85 247)"
+          ariaLabel="loading"
+        />
+      </p>
+    );
   }
 
   if (isSignedIn) {
